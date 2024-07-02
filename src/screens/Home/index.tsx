c99a6b1c0a66d37250f4f1a9823eb5e608287d93
Administrador <lucasgrigol@gmail.com>
2024-07-01 23:10:20 -0300

    feat: adicionado componente HomeHeader

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
+import { Text } from "react-native";
 import { Container } from "./styles";
+import { HomeHeader } from "../../components/HomeHeader";
 
 export function Home() {
     return (
         <Container>
-            
+            <HomeHeader />
         </Container>
     )
 }
