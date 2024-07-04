19fc3e7e6cf26c0ca14e24cde2fc790c96f016c3
Administrador <lucasgrigol@gmail.com>
2024-07-03 22:41:56 -0300

    feat: adicionado navegação com React Navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { AppProvider, UserProvider } from '@realm/react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { SignIn } from './src/screens/SignIn';
 import theme from './src/theme';
 import { Loading } from './src/components/Loading';
 import { StatusBar } from 'react-native';
 import { REALM_APP_ID } from '@env';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -20,14 +21,16 @@ export default function App() {
   return (
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
-        <StatusBar 
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <UserProvider fallback={SignIn}>
-          <Home />
-        </UserProvider>
+        <SafeAreaProvider>
+          <StatusBar 
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <UserProvider fallback={SignIn}>
+            <Routes />
+          </UserProvider>
+        </SafeAreaProvider>
       </ThemeProvider>
     </AppProvider>
   );
--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,20 +1,23 @@
 import { TouchableOpacity } from "react-native";
 import { Power } from "phosphor-react-native";
 import { useUser, useApp } from "@realm/react";
-
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Container, Greeting, Message, Name, Picture } from "./styles";
 import theme from "../../theme";
 
 export function HomeHeader() {
     const user = useUser();
     const app = useApp();
+    const insets = useSafeAreaInsets();
+
+    const paddingTop = insets.top + 32;
 
     function handleLogout() {
         app.currentUser?.logOut();
     }
 
     return (
-        <Container>
+        <Container style={{ paddingTop }}>
             <Picture 
                 source={{ uri: user?.profile.pictureUrl}}
                 placeholder="L184i9offQof00ayfQay~qj[fQj["
